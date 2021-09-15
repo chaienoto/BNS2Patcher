635323063ee14b1d9944ffe00f9a76c2d7b859b2
chaienoto <50574624+chaienoto@users.noreply.github.com>
2021-09-16 00:12:52 +0700

    Update bns2_eng_patcher.bat

--- bns2_eng_patcher.bat
+++ bns2_eng_patcher.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGmW+0UiKRYUag2OOXj6Tq1EvqHHwcm0jWspaMBtRIbY1brAKeMcig==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAjk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJgZk8aGmQ=
-::ZQ05rAF9IBncCkqN+0xwdVsEAlTMbCXrZg==
-::ZQ05rAF9IAHYFVzEqQISJggGDCGMO2ezFb1cxef/jw==
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATE2U00aBlNDCyNMGqpEroOpKjI7PiOslkYUKI9Yc/4z7uAJ/QLig==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBhBQwqFAE+/Fb4I5/jHwe+Q4ksSWOY6at7n2Lqdct8W5ELofIUl6WpbjMgECQkWewquDg==
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 mode con:cols=80 lines=30
 echo "       ____  _           _                        _                   _      "
