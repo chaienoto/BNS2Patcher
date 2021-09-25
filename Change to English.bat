504354e5d339a9d5a5ee479cb7e5dc694ccf93e3
chaienoto <50574624+chaienoto@users.noreply.github.com>
2021-09-25 11:22:55 +0700

    Delete Change to English.bat

--- Change to English.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Change Language option
-echo "Language option"
-for /f "delims=" %%b in ('powershell $env:LOCALAPPDATA') do set "json=%%b\B2\Saved\Option"
-powershell -Command "& {$a = Get-Content "%json%\LocalOption.json" -raw | ConvertFrom-Json; $a.loginOption.language = 22; $a | ConvertTo-Json | Set-Content "%json%\LocalOption.json";}"
-cd %json%
-powershell ls *.json
-echo "Language Change Succeeded!!"
-pause
